ce70f36347d6eabb422709c904cd53587c6fce91
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-07-11 09:16:50 -0700

    removed default ref

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const resolve = require('./lib').default;
+const resolve = require('./lib');
 
 function requirer(file) {
 	let router = require(file);
